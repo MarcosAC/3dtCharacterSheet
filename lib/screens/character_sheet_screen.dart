ed177015d5018533d119429526deaef5476dad37
Marcos Aurelio <marcos.aucorrea@gmail.com>
2023-05-31 22:01:40 -0300

    Adicionado o widget Card nos campos

--- lib/screens/character_sheet_screen.dart
+++ lib/screens/character_sheet_screen.dart
@@ -1,7 +1,6 @@
 import 'package:dtcharactersheet/widgets/characteristic_item.dart';
+import 'package:dtcharactersheet/widgets/characteristics.dart';
 import 'package:flutter/material.dart';
-
-import '../widgets/characteristics.dart';
 
 class CharacterSheetScreen extends StatefulWidget {
   const CharacterSheetScreen({super.key});
@@ -29,29 +28,36 @@ class _CharacterSheetScreenState extends State<CharacterSheetScreen> {
           padding: const EdgeInsets.all(8.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "Nome",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
+            children: const [
+              Card(
+                elevation: 1,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                child: Padding(
+                  padding: EdgeInsets.only(left: 10, right: 10),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: "Nome",
+                      border: InputBorder.none,
+                    ),
+                  ),
                 ),
               ),
-              const SizedBox(height: 10),
-              const Characteristics(tittle: 'Características', item: [
+              SizedBox(height: 10),
+              Characteristics(tittle: 'Características', item: [
                 CharacteristicItem(textItem: 'Força'),
                 CharacteristicItem(textItem: 'Habilidade'),
                 CharacteristicItem(textItem: 'Resitência'),
                 CharacteristicItem(textItem: 'Armadura'),
                 CharacteristicItem(textItem: 'Poder de Fogo')
               ]),
-              const SizedBox(height: 10),
-              const Characteristics(tittle: 'Tipos de Dano', item: [
+              SizedBox(height: 10),
+              Characteristics(tittle: 'Tipos de Dano', item: [
                 CharacteristicItem(textItem: 'Força'),
                 CharacteristicItem(textItem: 'Poder de Fogo')
               ]),
-              const SizedBox(height: 10),
-              const Characteristics(tittle: 'Caminhos da Magia', item: [
+              SizedBox(height: 10),
+              Characteristics(tittle: 'Caminhos da Magia', item: [
                 CharacteristicItem(textItem: "Água"),
                 CharacteristicItem(textItem: "Ar"),
                 CharacteristicItem(textItem: "Fogo"),
@@ -59,14 +65,14 @@ class _CharacterSheetScreenState extends State<CharacterSheetScreen> {
                 CharacteristicItem(textItem: "Terra"),
                 CharacteristicItem(textItem: "Trevas")
               ]),
-              const SizedBox(height: 10),
-              const Characteristics(tittle: 'Desvantagens', item: []),
-              const SizedBox(height: 10),
-              const Characteristics(tittle: 'Magias Conhecidas', item: []),
-              const SizedBox(height: 10),
-              const Characteristics(tittle: 'Dinheiro e Itens', item: []),
-              const SizedBox(height: 10),
-              const Characteristics(tittle: 'História', item: []),
+              SizedBox(height: 10),
+              Characteristics(tittle: 'Desvantagens', item: []),
+              SizedBox(height: 10),
+              Characteristics(tittle: 'Magias Conhecidas', item: []),
+              SizedBox(height: 10),
+              Characteristics(tittle: 'Dinheiro e Itens', item: []),
+              SizedBox(height: 10),
+              Characteristics(tittle: 'História', item: []),
             ],
           ),
         ),

6880c03a283d66911c54f544574d89a55ed0d74e
Marcos Aurelio <marcos.aucorrea@gmail.com>
2023-06-14 12:03:28 -0300

    incremente e decremento botao + e -

--- lib/widgets/characteristic_item.dart
+++ lib/widgets/characteristic_item.dart
@@ -1,27 +1,50 @@
 import 'package:flutter/material.dart';
 
-class CharacteristicItem extends StatelessWidget {
+class CharacteristicItem extends StatefulWidget {
   const CharacteristicItem({super.key, required this.textItem});
 
   final String textItem;
 
   @override
+  State<CharacteristicItem> createState() => _CharacteristicItemState();
+}
+
+class _CharacteristicItemState extends State<CharacteristicItem> {
+  final _itemTextController = TextEditingController();
+  int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _itemTextController.text = '$_counter';
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-      Padding(padding: const EdgeInsets.only(bottom: 3), child: Text(textItem)),
+      Padding(
+          padding: const EdgeInsets.only(bottom: 3),
+          child: Text(widget.textItem)),
       Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              if (_counter > 0) _decrementCounter();
+            },
             icon: const Icon(Icons.remove_circle, color: Colors.red),
           ),
-          const SizedBox(
+          SizedBox(
             width: 100,
             height: 35,
             child: TextField(
+              controller: _itemTextController,
+              keyboardType: TextInputType.number,
               textAlign: TextAlign.center,
-              decoration: InputDecoration(
+              onChanged: (value) {
+                _counter = int.tryParse(_itemTextController.text)!;
+              },
+              decoration: const InputDecoration(
                 contentPadding: EdgeInsets.all(10),
                 border: OutlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(5))),
@@ -29,11 +52,27 @@ class CharacteristicItem extends StatelessWidget {
             ),
           ),
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              _incrementCounter();
+            },
             icon: const Icon(Icons.add_circle, color: Colors.blue),
           ),
         ],
       )
     ]);
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      _itemTextController.text = '$_counter';
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+      _itemTextController.text = '$_counter';
+    });
   }
 }

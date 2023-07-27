8fb9ab5dcb955a64ea0723f4d8c9384b826faa95
Marcos Aurelio <marcos.aucorrea@gmail.com>
2023-07-27 16:02:02 -0300

    Navega enviaando dados da lista para a tela de ficha do personagem

--- lib/screens/character_sheet_screen.dart
+++ lib/screens/character_sheet_screen.dart
@@ -9,13 +9,19 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class CharacterSheetScreen extends StatefulWidget {
-  const CharacterSheetScreen({super.key});
+  const CharacterSheetScreen({super.key, this.character});
+
+  final Character? character;
 
   @override
-  State<CharacterSheetScreen> createState() => _CharacterSheetScreenState();
+  State<CharacterSheetScreen> createState() => _CharacterSheetScreenState(character);
 }
 
 class _CharacterSheetScreenState extends State<CharacterSheetScreen> {
+  _CharacterSheetScreenState(this.character);
+
+  final Character? character;
+
   bool isVisible = false;
 
   final _nomeController = TextEditingController();
@@ -42,219 +48,236 @@ class _CharacterSheetScreenState extends State<CharacterSheetScreen> {
   final _experienceController = TextEditingController();
 
   @override
+  void initState() {
+    super.initState();
+
+    if (character != null) {
+      _nomeController.text = character!.name;
+      _forceController.text = character!.force.toString();
+      _abilityController.text = character!.ability.toString();
+      _resistanceController.text = character!.resistance.toString();
+      _armorController.text = character!.armor.toString();
+      _firePowersController.text = character!.firePower.toString();
+      _healthPointsController.text = character!.healthPoints.toString();
+      _magicPointsController.text = character!.magicPoints.toString();
+      _forceDamageController.text = character!.forceDamage!;
+      _firePowerDamageController.text = character!.firePowerDamage!;
+      _waterController.text = character!.water.toString();
+      _airController.text = character!.air.toString();
+      _fireController.text = character!.fire.toString();
+      _lightController.text = character!.light.toString();
+      _earthController.text = character!.earth.toString();
+      _darknessController.text = character!.darkness.toString();
+      _advantageController.text = character!.advantage!;
+      _disadvantageController.text = character!.disadvantage!;
+      _spellsController.text = character!.spells!;
+      _historyController.text = character!.history!;
+      _moneyController.text = character!.moneyItems!;
+      _experienceController.text = character!.experience.toString();
+    } else {
+      _forceController.text = '0';
+      _abilityController.text = '0';
+      _resistanceController.text = '0';
+      _armorController.text = '0';
+      _firePowersController.text = '0';
+      _healthPointsController.text = '0';
+      _magicPointsController.text = '0';
+      _waterController.text = '0';
+      _airController.text = '0';
+      _fireController.text = '0';
+      _lightController.text = '0';
+      _earthController.text = '0';
+      _darknessController.text = '0';
+      _experienceController.text = '0';
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    _forceController.text = '0';
-    _abilityController.text = '0';
-    _resistanceController.text = '0';
-    _armorController.text = '0';
-    _firePowersController.text = '0';
-    _healthPointsController.text = '0';
-    _magicPointsController.text = '0';
-    _waterController.text = '0';
-    _airController.text = '0';
-    _fireController.text = '0';
-    _lightController.text = '0';
-    _earthController.text = '0';
-    _darknessController.text = '0';
-    _experienceController.text = '0';
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Ficha do Personagem - 3D&T"),
-        titleTextStyle: const TextStyle(fontSize: 17),
-        actions: [
-          //IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
-          IconButton(
-              onPressed: () {
-                Character character = Character(
-                  name: _nomeController.text,
-                  force: int.parse(_forceController.text),
-                  ability: int.parse(_abilityController.text),
-                  resistance: int.parse(_resistanceController.text),
-                  armor: int.parse(_armorController.text),
-                  firePower: int.parse(_firePowersController.text),
-                  healthPoints: int.parse(_healthPointsController.text),
-                  magicPoints: int.parse(_magicPointsController.text),
-                  forceDamage: _forceDamageController.text,
-                  firePowerDamage: _firePowerDamageController.text,
-                  water: int.parse(_waterController.text),
-                  air: int.parse(_airController.text),
-                  fire: int.parse(_fireController.text),
-                  light: int.parse(_lightController.text),
-                  earth: int.parse(_earthController.text),
-                  darkness: int.parse(_darknessController.text),
-                  advantage: _advantageController.text,
-                  disadvantage: _disadvantageController.text,
-                  spells: _spellsController.text,
-                  moneyItems: _moneyController.text,
-                  history: _historyController.text,
-                  experience: int.parse(_experienceController.text),
-                );
-                Provider.of<CharacterProvider>(context, listen: false).addCharacter(character);
-                Navigator.of(context).popAndPushNamed(AppRoutes.listCharacterScreen);
-              },
-              icon: const Icon(Icons.save)),
-          IconButton(
-              onPressed: () {
-                Navigator.of(context).popAndPushNamed(AppRoutes.listCharacterScreen);
-              },
-              icon: const Icon(Icons.list))
-          //PopupMenuButton(itemBuilder: (BuildContext context) => <PopupMenuEntry>[]),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Card(
-                elevation: 1,
-                shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: TextFieldCustom(
-                    label: 'Nome',
-                    textController: _nomeController,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
+        appBar: AppBar(
+          title: const Text("Ficha do Personagem - 3D&T"),
+          titleTextStyle: const TextStyle(fontSize: 17),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Character character = Character(
+                    name: _nomeController.text,
+                    force: int.parse(_forceController.text),
+                    ability: int.parse(_abilityController.text),
+                    resistance: int.parse(_resistanceController.text),
+                    armor: int.parse(_armorController.text),
+                    firePower: int.parse(_firePowersController.text),
+                    healthPoints: int.parse(_healthPointsController.text),
+                    magicPoints: int.parse(_magicPointsController.text),
+                    forceDamage: _forceDamageController.text,
+                    firePowerDamage: _firePowerDamageController.text,
+                    water: int.parse(_waterController.text),
+                    air: int.parse(_airController.text),
+                    fire: int.parse(_fireController.text),
+                    light: int.parse(_lightController.text),
+                    earth: int.parse(_earthController.text),
+                    darkness: int.parse(_darknessController.text),
+                    advantage: _advantageController.text,
+                    disadvantage: _disadvantageController.text,
+                    spells: _spellsController.text,
+                    moneyItems: _moneyController.text,
+                    history: _historyController.text,
+                    experience: int.parse(_experienceController.text),
+                  );
 
-              // Características
-              Characteristics(tittle: 'Características', item: [
-                CharacteristicItem(
-                  textItem: 'Força',
-                  itemTextController: _forceController,
-                ),
-                CharacteristicItem(
-                  textItem: 'Habilidade',
-                  itemTextController: _abilityController,
-                ),
-                CharacteristicItem(
-                  textItem: 'Resitência',
-                  itemTextController: _resistanceController,
-                ),
-                CharacteristicItem(
-                  textItem: 'Armadura',
-                  itemTextController: _armorController,
-                ),
-                CharacteristicItem(
-                  textItem: 'Poder de Fogo',
-                  itemTextController: _firePowersController,
-                )
-              ]),
-              const SizedBox(height: 10),
-
-              // Tipos de Dano
-              Characteristics(tittle: 'Tipos de Dano', item: [
-                const SizedBox(height: 5),
-                TextFieldCustom(
-                  label: 'Força',
-                  textController: _forceDamageController,
-                ),
-                const SizedBox(height: 10),
-                TextFieldCustom(
-                  label: 'Poder de Fogo',
-                  textController: _firePowerDamageController,
-                )
-              ]),
-              const SizedBox(height: 10),
-
-              // Experiência
-              Characteristics(tittle: 'Experiência', item: [
-                CharacteristicItem(
-                  textItem: 'Experiência',
-                  itemTextController: _experienceController,
-                )
-              ]),
-              const SizedBox(height: 10),
-
-              // Pontos de Vida
-              Characteristics(tittle: 'Pontos de Vida', item: [
-                CharacteristicItem(
-                  textItem: 'Pontos de Vida',
-                  itemTextController: _healthPointsController,
-                )
-              ]),
-              const SizedBox(height: 10),
-
-              // Pontos de Magia
-              Characteristics(tittle: 'Pontos de Magia', item: [
-                CharacteristicItem(
-                  textItem: 'Pontos de Magia',
-                  itemTextController: _magicPointsController,
-                )
-              ]),
-              const SizedBox(height: 10),
-
-              // Caminhos da Magia
-              Characteristics(tittle: 'Caminhos da Magia', item: [
-                CharacteristicItem(
-                  textItem: 'Água',
-                  itemTextController: _waterController,
-                ),
-                CharacteristicItem(
-                  textItem: 'Ar',
-                  itemTextController: _airController,
-                ),
-                CharacteristicItem(
-                  textItem: 'Fogo',
-                  itemTextController: _fireController,
-                ),
-                CharacteristicItem(
-                  textItem: 'Luz',
-                  itemTextController: _lightController,
-                ),
-                CharacteristicItem(
-                  textItem: 'Terra',
-                  itemTextController: _earthController,
-                ),
-                CharacteristicItem(
-                  textItem: 'Trevas',
-                  itemTextController: _darknessController,
-                )
-              ]),
-              const SizedBox(height: 10),
-
-              // Vantagens
-              Characteristics(
-                tittle: 'Vantagens',
-                item: [MultLineTextField(textController: _advantageController)],
-              ),
-              const SizedBox(height: 10),
-
-              // Desvantagens
-              Characteristics(
-                tittle: 'Desvantagens',
-                item: [MultLineTextField(textController: _disadvantageController)],
-              ),
-              const SizedBox(height: 10),
-
-              // Magias Conhecidas
-              Characteristics(
-                tittle: 'Magias Conhecidas',
-                item: [MultLineTextField(textController: _spellsController)],
-              ),
-              const SizedBox(height: 10),
-
-              // Dinheiro e Itens
-              Characteristics(
-                tittle: 'Dinheiro e Itens',
-                item: [MultLineTextField(textController: _moneyController)],
-              ),
-              const SizedBox(height: 10),
-
-              // História
-              Characteristics(
-                tittle: 'História',
-                item: [MultLineTextField(textController: _historyController)],
-              ),
-            ],
-          ),
+                  Provider.of<CharacterProvider>(context, listen: false).addCharacter(character);
+                  Navigator.of(context).popAndPushNamed(AppRoutes.listCharacterScreen);
+                },
+                icon: const Icon(Icons.save)),
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context).popAndPushNamed(AppRoutes.listCharacterScreen);
+                },
+                icon: const Icon(Icons.list))
+          ],
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+            child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+                  Card(
+                      elevation: 1,
+                      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+                      child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: TextFieldCustom(
+                            label: 'Nome',
+                            textController: _nomeController,
+                          ))),
+
+                  const SizedBox(height: 10),
+
+                  // Características
+                  Characteristics(tittle: 'Características', item: [
+                    CharacteristicItem(
+                      textItem: 'Força',
+                      itemTextController: _forceController,
+                    ),
+                    CharacteristicItem(
+                      textItem: 'Habilidade',
+                      itemTextController: _abilityController,
+                    ),
+                    CharacteristicItem(
+                      textItem: 'Resitência',
+                      itemTextController: _resistanceController,
+                    ),
+                    CharacteristicItem(
+                      textItem: 'Armadura',
+                      itemTextController: _armorController,
+                    ),
+                    CharacteristicItem(
+                      textItem: 'Poder de Fogo',
+                      itemTextController: _firePowersController,
+                    )
+                  ]),
+
+                  const SizedBox(height: 10),
+
+                  // Tipos de Dano
+                  Characteristics(tittle: 'Tipos de Dano', item: [
+                    const SizedBox(height: 5),
+                    TextFieldCustom(
+                      label: 'Força',
+                      textController: _forceDamageController,
+                    ),
+                    const SizedBox(height: 10),
+                    TextFieldCustom(
+                      label: 'Poder de Fogo',
+                      textController: _firePowerDamageController,
+                    )
+                  ]),
+
+                  const SizedBox(height: 10),
+
+                  // Experiência
+                  Characteristics(tittle: 'Experiência', item: [
+                    CharacteristicItem(
+                      textItem: 'Experiência',
+                      itemTextController: _experienceController,
+                    )
+                  ]),
+
+                  const SizedBox(height: 10),
+
+                  // Pontos de Vida
+                  Characteristics(tittle: 'Pontos de Vida', item: [
+                    CharacteristicItem(
+                      textItem: 'Pontos de Vida',
+                      itemTextController: _healthPointsController,
+                    )
+                  ]),
+
+                  const SizedBox(height: 10),
+
+                  // Pontos de Magia
+                  Characteristics(tittle: 'Pontos de Magia', item: [
+                    CharacteristicItem(
+                      textItem: 'Pontos de Magia',
+                      itemTextController: _magicPointsController,
+                    )
+                  ]),
+
+                  const SizedBox(height: 10),
+
+                  // Caminhos da Magia
+                  Characteristics(tittle: 'Caminhos da Magia', item: [
+                    CharacteristicItem(
+                      textItem: 'Água',
+                      itemTextController: _waterController,
+                    ),
+                    CharacteristicItem(
+                      textItem: 'Ar',
+                      itemTextController: _airController,
+                    ),
+                    CharacteristicItem(
+                      textItem: 'Fogo',
+                      itemTextController: _fireController,
+                    ),
+                    CharacteristicItem(
+                      textItem: 'Luz',
+                      itemTextController: _lightController,
+                    ),
+                    CharacteristicItem(
+                      textItem: 'Terra',
+                      itemTextController: _earthController,
+                    ),
+                    CharacteristicItem(
+                      textItem: 'Trevas',
+                      itemTextController: _darknessController,
+                    )
+                  ]),
+
+                  const SizedBox(height: 10),
+
+                  // Vantagens
+                  Characteristics(tittle: 'Vantagens', item: [MultLineTextField(textController: _advantageController)]),
+
+                  const SizedBox(height: 10),
+
+                  // Desvantagens
+                  Characteristics(tittle: 'Desvantagens', item: [MultLineTextField(textController: _disadvantageController)]),
+
+                  const SizedBox(height: 10),
+
+                  // Magias Conhecidas
+                  Characteristics(tittle: 'Magias Conhecidas', item: [MultLineTextField(textController: _spellsController)]),
+
+                  const SizedBox(height: 10),
+
+                  // Dinheiro e Itens
+                  Characteristics(tittle: 'Dinheiro e Itens', item: [MultLineTextField(textController: _moneyController)]),
+
+                  const SizedBox(height: 10),
+
+                  // História
+                  Characteristics(tittle: 'História', item: [MultLineTextField(textController: _historyController)]),
+                ]))));
   }
 }
 
--- lib/screens/list_character_screen.dart
+++ lib/screens/list_character_screen.dart
@@ -1,4 +1,6 @@
+import 'package:dtcharactersheet/models/character.dart';
 import 'package:dtcharactersheet/providers/character_provider.dart';
+import 'package:dtcharactersheet/screens/character_sheet_screen.dart';
 import 'package:dtcharactersheet/utils/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -11,25 +13,23 @@ class ListCharacterScreen extends StatefulWidget {
 }
 
 class _ListCharacterScreenState extends State<ListCharacterScreen> {
+  final List<Character> charactersList = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Lista de Personagens'),
-          titleTextStyle: const TextStyle(fontSize: 17),
-          actions: [
-            IconButton(
-                onPressed: () {
-                  Navigator.of(context).popAndPushNamed(AppRoutes.characterSheetScreen);
-                },
-                icon: const Icon(Icons.add)),
-            //   IconButton(
-            //       onPressed: () {
-            //         Provider.of<CharacterProvider>(context, listen: false).loadCharacters();
-            //       },
-            //       icon: const Icon(Icons.list))
-          ],
-        ),
+        appBar: AppBar(title: const Text('Lista de Personagens'), titleTextStyle: const TextStyle(fontSize: 17), actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.of(context).popAndPushNamed(AppRoutes.characterSheetScreen);
+              },
+              icon: const Icon(Icons.add)),
+          //   IconButton(
+          //       onPressed: () {
+          //         Provider.of<CharacterProvider>(context, listen: false).loadCharacters();
+          //       },
+          //       icon: const Icon(Icons.list))
+        ]),
         body: FutureBuilder(
             future: Provider.of<CharacterProvider>(context, listen: false).loadCharacters(),
             builder: (context, snapshot) => snapshot.connectionState == ConnectionState.waiting
@@ -40,13 +40,18 @@ class _ListCharacterScreenState extends State<ListCharacterScreen> {
                         : ListView.builder(
                             itemBuilder: (BuildContext context, int index) {
                               return Card(
-                                child: ListTile(
-                                  leading: const CircleAvatar(child: Text('P')),
-                                  title: Text('Personagen: ${characters.characterByIndex(index).name}'),
-                                  subtitle: Text(
-                                      'Pontos de Vida: ${characters.characterByIndex(index).healthPoints}\nXP: ${characters.characterByIndex(index).experience}'),
-                                ),
-                              );
+                                  child: ListTile(
+                                      leading: const CircleAvatar(child: Text('P')),
+                                      title: Text('Personagen: ${characters.characterByIndex(index).name}'),
+                                      subtitle: Text(
+                                          'Pontos de Vida: ${characters.characterByIndex(index).healthPoints}\nXP: ${characters.characterByIndex(index).experience}'),
+                                      onTap: () {
+                                        Navigator.pushReplacement(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: ((context) => CharacterSheetScreen(character: characters.characterByIndex(index)))),
+                                        );
+                                      }));
                             },
                             itemCount: characters.itemsCount,
                           ),
